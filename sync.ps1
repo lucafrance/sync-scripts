2471f5d654765c0ad8597deb9b932f4699069096
Luca Franceschini <luca.france@outlook.com>
2022-03-31 22:45:25 +0200

    Add PowerShell Transcript

--- sync.ps1
+++ sync.ps1
@@ -1,4 +1,6 @@
+Start-Transcript -Path "sync.log" -Append
 Set-Location -Path $PSScriptRoot
 python sync.py
 git -C ../sync-files add *
 git -C ../sync-files commit -m "Update files"
+Stop-Transcript
