72cad53fc5400bf09eef375dac6ab5f5b530892e
Luca Franceschini <luca.france@outlook.com>
2022-04-02 12:31:46 +0200

    Fix Start-Transcript
    Location must be set first, otherwise sync.log is created in the current 
    working directory

--- sync.ps1
+++ sync.ps1
@@ -1,5 +1,5 @@
-Start-Transcript -Path "sync.log" -Append
 Set-Location -Path $PSScriptRoot
+Start-Transcript -Path "sync.log" -Append
 python sync.py
 git -C ../sync-files add *
 git -C ../sync-files commit -m "Update files"
